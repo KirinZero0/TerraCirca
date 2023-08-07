98bbf9045d59a6c79b6f4aafdeaddeca3246288c
KirinZero0 <aryapermana02@gmail.com>
2023-08-07 12:19:06 +0800

    rev

--- resources/views/customer/order2.blade.php
+++ resources/views/customer/order2.blade.php
@@ -60,26 +60,25 @@
                     @endforelse
                 
                     @forelse($menus->where('type', \App\Models\Menu::MINUMAN) as $menu)
-                        <div class="bg-white p-4 rounded-lg shadow-md mb-5 border-b-4 border-[#4693B1]">
-                            <img src="{{  $menu->getImageUrl() }}" alt="Item 1" class="mb-4 w-full h-32 object-cover rounded-t-lg">
-                            <div class="p-2">
-                                <h3 class="text-xl font-semibold mb-2">{{$menu->name}}</h3>
-                                <p class="text-gray-600 text-sm">{{$menu->description}}</p>
-                                <p class="text-gray-600 text-sm">{{ formatRupiah($menu->price) }}</p>
-                            </div>
-                            <form action="{{ route('customer.add.order')}}" enctype="multipart/form-data" method="post"
+                    <div class="bg-white p-4 rounded-lg shadow-md mb-3 border-b-4 border-[#4693B1] relative">
+                        <img src="{{ $menu->getImageUrl() }}" alt="Item 1" class="mb-4 w-full h-32 object-cover rounded-t-lg">
+                        <div class="p-2">
+                            <h3 class="text-xl font-semibold mb-2">{{ $menu->name }}</h3>
+                            <p class="text-gray-600 text-sm">{{ $menu->description }}</p>
+                            <p class="text-gray-600 text-sm">{{ formatRupiah($menu->price) }}</p>
+                        </div>
+                        <form action="{{ route('customer.add.order') }}" enctype="multipart/form-data" method="post"
                             class="relative" novalidate onkeydown="return event.key !== 'Enter';">
                             @csrf
-                                <input type="hidden" name="reservation_id" value="{{$reservation->id}}">
-                                <input type="hidden" name="menu_id" value="{{$menu->id}}">
-                                <input type="number" name="quantity" class="w-16 text-center border rounded-md mr-2" value="1"  min="1">
-                                <div>
-                                    <button type="submit" class="bg-[#4693B1] text-white p-1 w-[3rem] h-[3rem] rounded-full hover:bg-blue-600 absolute bottom-[-1.5rem] right-[-1.5rem]">
-                                        <i class="fa-regular fa-plus"></i>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+                            <input type="hidden" name="reservation_id" value="{{ $reservation->id }}">
+                            <input type="hidden" name="menu_id" value="{{ $menu->id }}">
+                            <input type="number" name="quantity" class="w-16 text-center border rounded-md mr-2" value="1" min="1">
+                        </form>
+                        <button type="submit"
+                            class="bg-[#4693B1] text-white p-1 w-[3rem] h-[3rem] rounded-full hover:bg-blue-600 absolute -bottom-2 -right-2">
+                            <i class="fa-regular fa-plus"></i>
+                        </button>
+                    </div>
                     @empty
                         <p class="text-center text-gray-600 mt-4">Tidak Ada Minuman Yang Tersedia</p>
                     @endforelse
